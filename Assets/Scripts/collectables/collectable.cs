749f4c6d05999fb1e628459a8ee1fd9fee154cb6
18kgordo <=>
2020-09-19 17:20:47 -0400

    demonstrating branching
    check title

---
+++ Assets/Scripts/collectables/collectable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(":)");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
