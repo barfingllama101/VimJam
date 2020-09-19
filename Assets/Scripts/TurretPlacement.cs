b41534c302b7cbe428080da7a636c23c8c23dc57
Julian <julianheuser@outlook.com>
2020-09-19 15:52:01 -0400

    Added turret placement
    The turretPlacement script lets you click to place buildings wherever the gameobject it's attatched to has collision.  Basically, you can just use a polygoncollider to define the areas where stuff can be placed.

---
+++ Assets/Scripts/TurretPlacement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class TurretPlacement : MonoBehaviour
+{
+    [SerializeField]
+    Camera cam;
+
+    [SerializeField]
+    GameObject turret;
+
+    PolygonCollider2D col;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = GetComponent<PolygonCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 spawnPos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y));
+            if (col.OverlapPoint(spawnPos))
+            {
+                Instantiate(turret, new Vector3(spawnPos.x, spawnPos.y, 0), Quaternion.identity);
+
+                //TODO: remove the placed object from the inventory
+            }
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        
+    }
+}
