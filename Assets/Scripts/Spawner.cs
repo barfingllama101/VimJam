33e3655a5156b8e5ced36582889e9444dde6b92d
Julian <julianheuser@outlook.com>
2020-09-20 12:51:35 -0400

    Wave manager so waves start, end, and progress properly
    Temporary "ready up" key is P which will start the next wave

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,21 +4,25 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField]
-    Waves wave;
-
-    [SerializeField]
+    public Waves wave;
     public EnemyPath path;                //Path that spawned enemies will go to
 
     // Start is called before the first frame update
     void Start()
     {
-        startWave();
     }
 
-    void startWave()
+    public bool isDone = false;
+
+
+    int phaseNum;
+    int phasesComplete;
+    public void startWave()
     {
-        for (int i = 0; i < wave.enemies.Length; i++)
+        isDone = false;
+        phasesComplete = 0;
+        phaseNum = wave.enemies.Length;
+        for (int i = 0; i < phaseNum; i++)
         {
             StartCoroutine(startSpawning(i));
         }
@@ -37,6 +41,14 @@ public class Spawner : MonoBehaviour
         {
             Instantiate(objectToSpawn, transform.position, Quaternion.identity).GetComponent<EnemyBase>().path = path;
             yield return new WaitForSeconds(spawnInterval);
+        }
+
+        phasesComplete++;
+
+        //Let the wavemanager know that this spawner has spawned everything!
+        if(phasesComplete == phaseNum)
+        {
+            isDone = true;
         }
     }
 }
