a75eaa11212fb095accc0d782a2eb142cd97946b
Julian <julianheuser@outlook.com>
2020-09-19 22:22:34 -0400

    Added Waves scriptable object
    Also changed spawners to work with them

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,25 +5,35 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     [SerializeField]
-    GameObject objectToSpawn;
+    Waves wave;
 
     [SerializeField]
-    EnemyPath path;
-
-    int spawnNum = 5;
-
-    float spawnInterval = .6f;
+    public EnemyPath path;                //Path that spawned enemies will go to
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(startSpawning());
+        startWave();
     }
 
-
-    IEnumerator startSpawning()
+    void startWave()
     {
-        for(int i = 0; i < spawnNum; i++)
+        for (int i = 0; i < wave.enemies.Length; i++)
+        {
+            StartCoroutine(startSpawning(i));
+        }
+    }
+
+    IEnumerator startSpawning(int num)
+    {
+        yield return new WaitForSeconds(wave.enemies[num].timeToAppear);
+
+        GameObject objectToSpawn = wave.enemies[num].enemyType;
+        objectToSpawn = wave.enemies[num].enemyType;
+        int spawnNum = wave.enemies[num].number;
+        float spawnInterval = wave.enemies[num].interval;
+
+        for (int i = 0; i < spawnNum; i++)
         {
             Instantiate(objectToSpawn, transform.position, Quaternion.identity).GetComponent<EnemyBase>().path = path;
             yield return new WaitForSeconds(spawnInterval);
---
+++ Assets/Waves/wavesScriptableObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct waveEnemy
+{
+    public GameObject enemyType;    //What type of enemy to spawn
+    public int number;              //How many enemies to spawn
+    public float interval;          //delay between each enemy spawn
+    public float timeToAppear;     //When the enemy appears
+}
+
+[CreateAssetMenu(fileName = "Waves", menuName = "Waves", order = 1)]
+public class Waves : ScriptableObject
+{
+    public waveEnemy[] enemies;
+}
