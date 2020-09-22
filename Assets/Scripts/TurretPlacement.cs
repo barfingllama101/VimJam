3fa273e348f23cf28a4765a2889ddb0980aef86a
Julian <julianheuser@outlook.com>
2020-09-22 16:52:14 -0400

    UI inventory - click and drag to place buildings

--- Assets/Scripts/TurretPlacement.cs
+++ Assets/Scripts/TurretPlacement.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEngine.UI;
 
 public class TurretPlacement : MonoBehaviour
 {
@@ -11,11 +12,18 @@ public class TurretPlacement : MonoBehaviour
     [SerializeField]
     TilemapCollider2D prohibitedArea;
 
-    public collectable buildings;
+    [SerializeField]
+    Transform content;
+
+    [SerializeField]
+    GameObject UIElement;
+
+    //public collectable buildings;
 
     // Update is called once per frame
     void Update()
     {
+        /*
         if (Input.GetMouseButtonDown(0))
         {
             if (Inventory.buildings.Contains(buildings))
@@ -29,17 +37,35 @@ public class TurretPlacement : MonoBehaviour
                 }
             }
         }
+        */
     }
 
-    public void setActiveBuilding(collectable building)
+    public void placeBuilding(collectable building)
     {
-        buildings = building;
+        if (Inventory.buildings.Contains(building))
+        {
+            Vector3 spawnPos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y));
+            if (!prohibitedArea.OverlapPoint(spawnPos))
+            {
+                Instantiate(building.building, new Vector3(spawnPos.x, spawnPos.y, 0), Quaternion.identity);
+
+                Inventory.buildings.Remove(building);
+
+                building = null;
+            }
+        }
+        else
+        {
+            Debug.Log("ERROR: This shouldn't be happening! Something's wrong! panic! AAAAA!");
+        }
     }
 
     //Test function
     public void add(collectable building)
     {
         Inventory.buildings.Add(building);
+        Inventory_UI ui = Instantiate(UIElement, content).GetComponent<Inventory_UI>();
+        ui.SetUp(Inventory.buildings.Count, building);
     }
 
 }
